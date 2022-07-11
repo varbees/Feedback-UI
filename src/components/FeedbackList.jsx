9d7d0d1effbec10a7cd128bd373a21e00ff5a5c5
harshavar-bit <harshavar968@gmail.com>
2022-07-11 13:20:47 +0530

    Animating Feedback Items with Framer-motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
@@ -9,9 +10,22 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 
   return (
     <div>
-      {feedback.map(item => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map(item => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
