ca35dba316bfee35274f01bf4ed269697ce50745
harshavar-bit <harshavar968@gmail.com>
2022-07-11 13:09:13 +0530

    Add new Feedback Form Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
@@ -12,10 +14,16 @@ const App = () => {
     }
   };
 
+  const addFeedback = newFeedback => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   return (
     <>
       <Header text='Feedback UI' />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
